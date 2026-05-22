05ce487d06298d89c917b7c8903757dc59af21f1
Kanzah <165935507+Kanshaaaa@users.noreply.github.com>
2026-05-22 22:52:36 +0300

    added images and logos. removed default code in App.jsx

---
+++ cocreate_website/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+      </ul>
+    </nav>
+  );
+};
